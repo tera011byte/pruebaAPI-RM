0c0cbcb7a9050304306f3503b6a8c16a8514f562
Cristobal Flores <terabyte@localhost.localdomain>
2021-02-23 19:07:40 -0600

    Utilizando React, css, sass y javascript consumimos el API de rick and morty para mostrar a los personajes, prueba 1 con javascript y consumo de API

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {Fragment} from 'react'
+import Header from './components/Header'
+import Characters from './components/Characters'
+import './main.sass'
+import './components/Header.css'
+
+const App = () => {
+    return ( 
+        <Fragment>
+            <Header 
+                title="Rick and Morty API"
+            />
+            <Characters />
+        </Fragment>
+     );
+}
+ 
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = ({title}) => {
+    return ( 
+        <header>
+            <div className="container">
+                <div className="content">
+                    <h1 className="title">{title}</h1>
+                    <p className="text">This is a proyect the Rick and Morty's characters </p>
+                </div>
+            </div>
+
+        </header>
+     );
+}
+
+export default Header;
